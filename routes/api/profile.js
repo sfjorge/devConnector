7af73a102ab7226f9badef5408c876754b9ff88f
unknown <jorgesantos214@gmail.com>
2020-06-04 11:15:01 -0400

    GET logged in user profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-//@route   GET api/profile
-//@desc    Test route
-//@access  Public
-router.get('/', (req, res) => res.send('Profile route'));
+const Profile = require('../../model/Profile');
+const User = require('../../model/User');
+
+//@route   GET api/profile/me
+//@desc    GET current users profile
+//@access  Private --> will require auth to access protected token
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
+
+        if(!profile) {
+            return res.status(500).json({ msg: 'There is no profile for this user'});
+        }
+        res.json(profile);
+    } catch (err) {
+        console.err(err.message);
+        res.status(500).send('Servor Error');
+    }
+}); // simply need to add 'auth' parameter to secure route
 
 module.exports = router;
