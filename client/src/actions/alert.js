e066f0b83f8254987ac97634084e4eb499a0d4fa
Santos <jorgesantos214@gmail.com>
2020-06-07 20:23:40 -0400

    Alert Reducer, Action & Type

---
+++ client/src/actions/alert.js
@@ -0,0 +1,10 @@
+import uuid from 'uuid';
+import { SET_AlERT, REMOVE_ALERT, SET_ALERT } from './types';
+
+export const setAlert = (msg, alertType) => dispatch => {
+    const id = uuid.v4();
+    dispatch({
+        type: SET_ALERT,
+        payload: { msg, alertType, id }
+    });
+}
